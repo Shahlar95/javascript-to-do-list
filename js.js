d3efd9a7542c58fbc90dcaf8043538a91aa2da6c
Shahlar95 <asahlar3@gmail.com>
2023-01-11 19:32:09 +0400

    tasklar/shahlar

---
+++ js.js
@@ -0,0 +1,98 @@
+//  let number1 = prompt();
+//  let number2 = prompt();
+
+//  let netice = number2**2/number1;
+
+//  if(Number.isInteger(netice)){
+
+//     console.log(netice);//  utf-16-ya  cevire bilmedim
+//  }else{
+//     console.log("netice alinmadi");
+//  }  
+
+// if(user == c){
+
+    //     alert("palindrom");
+    // }else{
+    //     alert("palindrom deyil");
+    // }
+    
+
+    // Palindrom helli
+
+//     let user = prompt();
+
+// function funksiya(a){
+
+//     const say = user.length;
+
+//     for(a=0; a<say/2; a++){
+
+//         if(user[a] !== user[say-1-a]){
+
+//             alert("palindrom deyil");
+//             break;
+//         }
+//         else {alert("palindromdur");
+//     break;}
+//     }
+
+   
+    
+// }
+
+
+// let value = funksiya(user);
+
+
+// daxil olunan a.e,u herflerini * evezlemek
+
+let text = prompt();
+
+let text1 = text.split("");
+
+
+let b = text1.replaceAll("a","*").c.d;
+let c = text1.replaceAll("e","*");
+let d = text1.replaceAll("u","*");
+
+console.log(b);
+
+
+
+
+
+// ekob ve ebob taski 
+
+// let x = prompt();
+// let y = prompt();
+
+// function Ekob_Ebob(x, y) {
+//     if ((typeof x !== 'number') || (typeof y !== 'number')) 
+//       return false;
+//     x = Math.abs(x);
+//     y = Math.abs(y);
+//     while(y) {
+//       var t = y;
+//       y = x % y;
+//       x = t;
+//     }
+//     return x;
+//   }
+
+
+//   console.log(Ekob_Ebob(x,y));
+
+
+
+// temperator taski
+
+// let c = prompt("temperator C-ile");
+
+// let f = c*9/5+32;
+
+// let k = (f+459.67)*5/9;
+
+// console.log(f + " Farenheyt");
+// console.log(k + " Kelvin");
+
